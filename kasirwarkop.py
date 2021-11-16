f543ee64fd8c51def10eddea7265e53a1b1f7461
putrideliaar <77769493+putrideliaar@users.noreply.github.com>
2021-11-16 09:40:16 +0700

    Add files via upload

---
+++ kasirwarkop.py
@@ -0,0 +1,49 @@
+#kasir warkop 
+print("WARKOP STORY")
+print("pilih menu: ")
+print("1. kopi susu -> 20.000")
+print("2. kopi capuchino -> 25.000")
+print("3. kopi ekspreso -> 30.000")
+print("4. kopi hitam murni -> 35.000")
+print("5. kopi spesial love story -> 45.000") 
+pilih = int(input("masukkan menu pilihan: "))
+if pilih == 1:
+    print("kopi susu dengan harga Rp. 20.000")
+    jumlah = int(input("jumlah pesanan : "))
+    print("")
+    Total = jumlah*20000
+    print("Total harga: ",Total)
+    print("=======Thank you======")
+elif pilih == 2:
+     print("kopi capuchino dengan harga Rp. 25.000")
+     jumlah = int(input("jumlah pesanan : "))
+     print("")
+     Total = jumlah*25000
+     print("Total harga: " ,Total)
+     print("======Thank you======")
+elif pilih == 3:
+    print("kopi ekspreso dengan harga Rp. 30.000")
+    jumlah = int(input("jumlah pesanan : "))
+    print("")
+    Total = jumlah*30000
+    print("Total harga: " ,Total)
+    print("=======Thank you======")
+elif pilih == 4:
+    print("kopi hitam murni dengan harga Rp. 35.000")
+    jumlah = int(input("jumlah pesanan : "))
+    print("")
+    Total = jumlah*35000
+    print("Total harga:" ,Total)
+    print("=======Thank you=======")
+elif pilih == 5:
+    print("kopi spesial love story dengan harga Rp. 45.000")
+    jumlah = int(input("jumlah pesanan : "))
+    print("")
+    Total = jumlah*45000
+    print("Total harga:" ,Total)
+    print("_=======Thank you======")
+     
+     
+     
+
+   
